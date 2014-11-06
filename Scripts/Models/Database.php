b10eb2fca3ca2a023ad0caa10cd01738dd8ec7cf
Joseph Alvizo Mendoza <josephalvizomendoza@Josephs-MacBook-Pro.local>
2014-11-05 21:17:31 -0600

    Finished Database Connection file

--- Scripts/Models/Database.php
+++ Scripts/Models/Database.php
@@ -1,38 +1,46 @@
 <?php
-class Database
-{
-    
-    private $host = "www.kinrally.com";
-    private $password = "utsa";
-    private $username = "Dealer";
-    private $dbName = "JimsCarDealerShip";
-    private $link;
-    public function __construct()
+    class database
     {
-        $link = mysql_connect($host, $username, $password);
-    }
-    public function query()
-    {
-        
-    }
-    public function getError()
-    {
-        
-    }
-    public function getErrorNo()
-    {
-        
-    }
-    public function printStatus()
-    {
-        if($link)
+        var $link;
+        public function __construct()
         {
-            print 'Connection Made';
+            $dbHost     = "www.kinrally.com";
+            $dbPassword = "utsa";
+            $dbUserName = "Dealer";
+            $this->link = mysql_connect($dbHost,$dbUserName,$dbPassword);
         }
-        else
+        public function getDBStatus()
         {
-            print 'Error in Connection';
+            if (!$this->link)
+            {
+                echo 'No database connection';
+            }
+            else
+            {
+                echo 'Connected successfully to database';
+            }
         }
-    }
-}
+        public function query($query)
+        {
+            mysql_select_db("JimsCarDealerShip",$this->link);
+            $reply = mysql_query($query,$this->link);
+            $errorno = mysql_errno($this -> link);
+            //$error = mysql_error($this -> link);
+            //print $error." : ".$query;
+            return $reply;
+        }
+        public function getErrorNumber()
+        {
+            return mysql_errno($this -> link);
+        }
+        public function getError()
+        {
+            return mysql_error($this -> link);
+        }
+        public function getLastID()
+		  {
+				$result = mysql_insert_id();
+		  		return $result;
+		  }
+	}
 ?>
