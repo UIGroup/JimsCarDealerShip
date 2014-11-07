49f017c6bda89c0f9b997240babc13adc2f918d3
Joseph Alvizo Mendoza <josephalvizomendoza@Josephs-MacBook-Pro.local>
2014-11-06 22:40:24 -0600

    Set spot for lester sechr stuff

--- Browse.php
+++ Browse.php
@@ -55,7 +55,10 @@
     <div class="container-fluid">
       <div class="row">
         <div class="col-sm-3 col-md-2 sidebar">
-          
+
+        
+        <!-- Search things go here -->
+        
         </div>
         <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main bootcards-cards">
           <!--<h2 class="page-header">Our Selection</h2>-->
